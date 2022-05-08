9fb65f0adebbf1e878fdce8593d720484339d04c
Nirayulab <81407593+Nirayulab@users.noreply.github.com>
2022-05-08 20:43:53 +0545

    lab work finish

---
+++ ds/lab_1/Q4.cpp
@@ -0,0 +1,26 @@
+/*
+Write a program that takes name and age of a 5 persons as an input and 
+gives the degree of membership of the person as its output according to 
+following membership functions.
+Perform set operations according to rules of fuzzy sets, on these 
+two sets.
+*/
+
+#include <stdio.h>
+#include "Q4.c"
+
+int main() {
+
+    char names[5][15];
+    int age[5], i, j;
+
+    for(i = 0; i<5; i++) {
+        printf("Enter name and age of the person: ");
+        scanf(" %s %d", names[i], &age[i]);
+    }
+
+    printQ4aFuzzySet(age, names);  
+    printQ4bFuzzySet(age, names);  
+
+    return 0;
+}
